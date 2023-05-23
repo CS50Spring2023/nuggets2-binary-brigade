53f0d1635ee0996632fc25ec5ac96a04ec86fdd4
Charlie Baker <charles.d.baker.24@dartmouth.edu>
2023-05-23 10:56:22 -0400

    first go at game module

---
+++ game/game.h
@@ -0,0 +1,9 @@
+/* 
+ * player.h - header file for CS50 player module
+ * 
+ * Binary Brigade, Spring, 2023
+ */
+
+
+/**************** global types ****************/
+typedef struct player player_t;  // opaque to users of the module
