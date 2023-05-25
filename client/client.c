d4770d38897cac3d69d92b03167fb344c283b2de
Kashan Mahmood <f004gq8@plank.thayer.dartmouth.edu>
2023-05-25 01:44:47 -0400

    she kinda works rn

--- client/client.c
+++ client/client.c
@@ -7,16 +7,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 #include <curses.h>
+#include <ncurses.h> 
 #include <unistd.h>
+#include <signal.h>  
 #include "../support/message.h"
 #include "../support/log.h"
 
-#define NR 20
-#define NC 20
 
-// GLOBAL VAR
-char* playername;
 
 // can i have nr and nc as globals??????
 
@@ -27,22 +26,43 @@ addr_t server_setup(char* hostname, char* port, char* playername);
 void handle_display(const char* message);
 void handle_quit(const char* message);
 void handle_error(const char* message);
-void update_status_line(int collected, int purse, int remaining);
-void initDisplay();
+WINDOW* initDisplay();
+
+
+
+typedef struct client_info{
+    int NR;
+    int NC;
+    int map_nr;
+    int map_nc;
+    int collected;
+    int purse;
+    int remaining;
+    WINDOW* win;
+    char* playername;
+    char playerletter;
+    char* last_display;
+} client_info_t;
+
+client_info_t* client_info;
 
 int main(int argc, char *argv[])
 {
     char* hostname;
     char* port;
-    playername = (argc == 4) ? argv[3] : NULL;
+    char* playername = (argc == 4) ? argv[3] : NULL;
 
     if (parseArgs(argc, argv, &hostname, &port, &playername) != 0) {
         printf("Usage: ./client hostname port [playername]\n");
         return 1;
     }
 
+    client_info = calloc(1, sizeof(client_info_t));
+
     // TODO/ PROBLEM initialize display
-    initDisplay();
+    client_info->win  = initDisplay();
+
+    client_info->playername = playername;
 
     // initalize network + join the game
     addr_t server = server_setup(hostname, port, playername);
@@ -52,7 +72,10 @@ int main(int argc, char *argv[])
 
     // clean up message modual
     message_done();
+    
+    delwin(client_info->win);
     endwin();
+    free(client_info);
     return ok? 0 : 10;
 }
 
@@ -65,137 +88,166 @@ handleMessage(void* arg, const addr_t from, const char* message)
 
     if (strcmp(messageType, "OK") == 0){
         
-        // OK message received, do nothing
+        sscanf(message, "%*s %c", &client_info->playerletter);
     
     } else if (strcmp(messageType, "GRID") == 0){
-
-        // still need to implement this
+    
         int nrows, ncols;
         sscanf(message, "%*s %d %d", &nrows, &ncols);
+        client_info->map_nc = ncols;
+        client_info->map_nr= nrows;
+        getmaxyx(stdscr, client_info->NR, client_info->NC);
 
-        if (nrows != NR + 1 || ncols != NC){
+        while (nrows +1 > client_info->NR || ncols > client_info->NC) {
             
-            handle_display("Please resize the window to fit the grid.");
-            while (getch() == ERR){
-                // keep going till user resizes the window
-            }
             clear();
+            mvprintw(0, 0, "Your window must be at least %d high", nrows+1);
+            mvprintw(1, 0, "Your window must be at least %d wide", ncols);
+            mvprintw(3, 0, "Current size: %d (height) x %d (width)", client_info->NR, client_info->NC);
+            mvprintw(2, 0, "Resize your window, and press Enter to continue.");
+           
+            refresh();
+
+            while (getch() != '\n') {
+                // Wait for Enter key press
+            }
+
+            getmaxyx(stdscr, client_info->NR, client_info->NC);
+            refresh();
+
         }
+
+        wresize(client_info->win, client_info->NR, client_info->NC);
+        mvprintw(7, 0, "Current size: %d (height) x %d (width)", client_info->NR, client_info->NC);
+        
         
     } else if (strcmp(messageType, "GOLD") == 0){
 
-        int collected;
-        int purse;
-        int remaining;
 
-        sscanf(message, "%*s %d %d %d", &collected, &purse, &remaining);
-        update_status_line(collected, purse, remaining);
+        sscanf(message, "%*s %d %d %d", &client_info->collected, &client_info->purse, &client_info->remaining);
+       
 
     } else if (strcmp(messageType, "DISPLAY") == 0){
-    
         handle_display(message);
 
     } else if (strcmp(messageType, "QUIT") == 0){
         
-        endwin(); 
-        printf("%s\n", message);
-        exit(0);
+        const char* spacePos = strchr(message, ' ');
+        if (spacePos != NULL) {
+            // Extract the remaining part of the message after the space
+            const char* explanation = spacePos + 1;
+
+            // Display the remaining message on the screen
+            clear();
+            mvprintw(0, 0, "Server message: %s", explanation);
+            refresh();
+        }
+        sleep(3);
+        return true;
     
     } else if (strcmp(messageType, "ERROR") == 0) {
 
-        endwin(); 
-        fprintf(stderr, "Server error: %s\n", message);
-        exit(1);
     
-    } else{
-
-        // malformed message
-        fprintf(stderr, "Malformed message from the server: %s\n", message);
-        refresh();
+        handle_error(message);
         
     }
 
     return false;
 }
 
-void 
-handle_display(const char* message)
+void handle_display(const char* message)
 {
+
+    // Skip the "DISPLAY" part of the message
+    const char* gridString = message + strlen("DISPLAY\n");
+
+    // Clear the window and move to the top-left corner
+    clear();
     move(1, 0);
-    clrtoeol();
-    printw("%s", message);
-    refresh();
-}
-void 
-update_status_line(int collected, int purse, int remaining) 
-{
-    // Construct the status line string
+        
+        
+    // Print each line of the grid string
+    while (*gridString != '\0')
+    {
+        printw("%.*s", client_info->NC, gridString);  // Print one line
+        gridString += client_info->NC;  // Move to the next line
+    }
+
+    // update status line
     char statusLine[256];
-    
-    if (playername != NULL) {
-        snprintf(statusLine, sizeof(statusLine), "Player %s has %d nuggets (%d nuggets unclaimed).",
-                 playername, purse, remaining);
+
+    if (client_info->playername != NULL) {
+        snprintf(statusLine, sizeof(statusLine), "Player %c has %d nuggets (%d nuggets unclaimed).\n",
+                 client_info->playerletter, client_info->purse, client_info->remaining);
     } else {
-        snprintf(statusLine, sizeof(statusLine), "Spectator: %d nuggets unclaimed.", remaining);
+        snprintf(statusLine, sizeof(statusLine), "Spectator: %d nuggets unclaimed.\n", client_info->remaining);
     }
 
     // Print the status line at the top of the screen
     move(0, 0);
     clrtoeol();
-    printw("%s", statusLine);
+    mvprintw(0, 0, "%s", statusLine);
+
+    char* tmp_display = (char*)message;
+    client_info->last_display = tmp_display;
+    
+    // Refresh the window to show the changes
     refresh();
+    
 }
+
+
+void 
+handle_error(const char* message) 
+{
+
+    const char* spacePos = strchr(message, ' ');
+    if (spacePos != NULL) {
+        // Extract the remaining part of the message after the space
+        const char* explanation = spacePos + 1;
+
+        // Display the remaining message on the screen
+        mvprintw(0, 0, "Server message -> %s", explanation);
+        refresh();
+    }
+    sleep(1);
+    handle_display(client_info->last_display);
+ 
+}
+
+
 
 bool 
 handleInput(void* arg) {
-    
     addr_t* server = arg;
-    
-    if (_SC_THREAD_SPORADIC_SERVER == NULL) { // defensive
-        
+
+    if (server == NULL) {
         log_v("handleInput called with arg=NULL");
         return true;
     }
-  
+
     if (!message_isAddr(*server)) {
-        
         log_v("handleInput called without a correspondent.");
         printf("You have no correspondent.\n");
         fflush(stdout);
         return false;
     }
 
-    char message[message_MaxBytes];
- 
-    if (fgets(message, message_MaxBytes, stdin) != NULL) {
-        
-        const int len = strlen(message);
+    int ch = getch();  // Read a single character of input using getch()
 
-        // PROBLEM: What if we have more than one key per line
-        if (len > 2){
-            // message cannot be more than on character (key is only one char?)
-            return true;
-            // print to stderr and continue TODO
-        
-        } else if (len > 0) {
-            // change newline to null
-            message[len-1] = '\0'; 
-        }
-
-        // format message
-        snprintf(message, message_MaxBytes, "KEY %s", message);
-
-        // send the line to our correspondent
-        message_send(*server, message);
-        return false;
-   
-    } else {
-        // EOF
-        return true; 
+    // Check if the character is a newline (Enter key)
+    if (ch == '\n') {
+        return false;  // Ignore newline character and continue waiting for input
     }
-    
-}
 
+    // Format the message with the key
+    char message[message_MaxBytes];
+    snprintf(message, message_MaxBytes, "KEY %c", ch);
+
+    // Send the message to the server
+    message_send(*server, message);
+    return false;
+}
 
 addr_t
 server_setup(char* hostname, char* port, char* playername)
@@ -246,16 +298,22 @@ int parseArgs(const int argc, char* argv[], char** hostname, char** port, char**
     return 0;
 }
 
-void
+WINDOW*
 initDisplay()
 {
     // Initialize curses
     initscr();
     cbreak();
     noecho();
-    // curs_set(0);
+    curs_set(0);
     refresh();
 
+    // starter we will update later
+    client_info->NR = 0;
+    client_info->NC = 0;
+
     // Create a new window for the game display
-    // WINDOW* win = newwin(NR + 1, NC, 0, 0);
+    WINDOW* win = newwin(client_info->NR + 1, client_info->NC, 0, 0);
+    refresh();
+    return win;
 }
