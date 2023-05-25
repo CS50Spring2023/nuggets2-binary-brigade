ab20b9c1072f9b5b00b7c558bf47220aabf05e15
Charlie Baker <charles.d.baker.24@dartmouth.edu>
2023-05-25 09:27:02 -0400

    edits to server

--- server/server.c
+++ server/server.c
@@ -14,7 +14,13 @@
 #include "../grid/grid.h"
 #include "../game/game.h"
 
-int main(int argc, char *argv[])
+/**************** file-local functions ****************/
+
+static bool handleMessage(void* arg, const addr_t from, const char* message);
+
+/***************** main *******************************/
+int 
+main(int argc, char *argv[])
 {
   if (argc != 2 || argc != 3){
     fprintf(stderr, "invalid number of arguments -- must have either 1 or 2 arguments (mapfile and randomSeed)\n");
@@ -39,12 +45,62 @@ int main(int argc, char *argv[])
   grid_t* grid = gridInit(argv[1], randomSeed);
   game_t* game = initialize_game(grid);
 
-
-  int portNumber = message_init(NULL);
-  if (portNumber == 0){
-    fprintf(stderr, "Error initializing network");
-    return 1;
+  // initialize the message module (without logging)
+  int myPort = message_init(NULL);
+  if (myPort == 0) {
+    return 2; // failure to initialize message module
+  } else {
+    printf("serverPort=%d\n", myPort);
   }
 
-  return 0;
+  bool ok = message_loop(NULL, 0, NULL, NULL, handleMessage);
+
+  // shut down the message module
+  message_done();
+  
+  return ok? 0 : 1; // status code depends on result of message_loop
+}
+
+/**************** handleMessage ****************/
+/* Datagram received; print it, read a line from stdin, and use it as reply.
+ * We ignore 'arg' here.
+ * Return true if EOF on stdin or any fatal error.
+ */
+static bool
+handleMessage(void* arg, const addr_t from, const char* message)
+{
+  // print the message and a prompt
+  printf("'%s'\n", message);
+  printf("> ");
+  fflush(stdout);
+
+  // allocate a buffer into which we can read a line of input
+  // (it can't be any longer than a message)!
+  char line[message_MaxBytes];
+
+  // read a line from stdin: this is VERY unusual for a handleMessage()
+  if (fgets(line, message_MaxBytes, stdin) == NULL) {
+    // EOF case: stop looping
+    putchar('\n');
+    return true;
+  } else {
+    // strip trailing newline
+    const int len = strlen(line);
+    if (len > 0 && line[len-1] == '\n') {
+      line[len-1] = '\0';
+    }
+    /**
+     * this is where I will process input
+     * 
+     * 
+     * 
+     * 
+    */
+
+    // send as message back to client
+    message_send(from, line);
+
+    // normal case: keep looping
+    return false;
+  }
 }
